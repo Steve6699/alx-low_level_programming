852692980483457fd83bbbefc43925d676a3ed63
Steve6699 <tifaalkhwad@gmail.com>
2023-08-21 19:39:25 -0700

    update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen - returnes the ength of a string
+ * _strlen - returnes the length of a string
  *
  * @s: string parameter input
  *
@@ -12,7 +12,7 @@ int _strlen(char *s)
 {
 	int counter;
 
-	fot (counter = 0; *s != '\0'; s++)
+	for  (counter = 0; *s != '\0'; s++)
 		++counter;
 	return (counter);
 }

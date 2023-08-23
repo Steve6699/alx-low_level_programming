50e9b19b202f8ac04791c08f444786e3767363ca
Steve6699 <tifaalkhwad@gmail.com>
2023-08-23 13:56:14 -0700

    test last diget of random number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: test last digit of random number
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n, dig;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	dig = n % 10;
+
+	if (dig > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, dig);
+	else if (dig == 0)
+		printf("Last digit of %d is %d and is 0\n", n, dig);
+	else if (dig < 6 && dig != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, dig);
+
+	return (0);
+}

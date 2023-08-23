569d30b67885bca41fe1e1ab2a5889bfedcd4e0f
Steve6699 <tifaalkhwad@gmail.com>
2023-08-23 10:01:44 -0700

    print quote with write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qouet using function write
+ * write(int fd.const void *bfr.size_t count)
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1.q.59);
+	return (1);
+}

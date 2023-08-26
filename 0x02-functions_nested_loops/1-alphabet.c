ca606856dc09150fcabd7ff430ab1557e1fc3bb9
Steve6699 <tifaalkhwad@gmail.com>
2023-08-25 21:28:55 -0700

    alphabet with _putchar function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabets
+ *
+ * Return: 0 (Succes)
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet with _putchar
+ *
+ * Return: 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+
+}

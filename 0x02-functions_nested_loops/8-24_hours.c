d55fdfdfd77efbecf6c5e4a97de1f7fab45208fc
Steve6699 <tifaalkhwad@gmail.com>
2023-08-27 17:23:32 -0700

    print all min in day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,15 +6,17 @@
 
 void jack_bauer(void)
 {
-	for (int hour = 0; hour <= 23; hour++)
+	int hour, min;
+
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (int min = 0; min <= 59; min++)
+		for (min = 0; min <= 59; min++)
 		{
-			_putchar(hour / 10 + 48);
-			_putchar(hour % 10 + 48);
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
 			_putchar(':');
-			_puchar(min / 10 + 48);
-			_putchar(min % 10 + 48);
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 		}
 	}

fbf3e89ccf42191dda8540d12126e12da62d4f37
Steve6699 <tifaalkhwad@gmail.com>
2023-08-27 20:35:02 -0700

    print table n times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int num, mult, proud;
 
-	if (n != 15 || n != 0)
+	if (n >= 0 && n <= 15)
 	{
 		for (num = 0; num <= n; num++)
 		{
@@ -22,13 +22,19 @@ void print_times_table(int n)
 				proud = num * mult;
 				if (proud <= 9)
 					_putchar(' ');
-				else
+				if (proud <= 99)
+					_putchar(' ');
+
+				if (proud >= 100)
+				{
+					_putchar((proud / 100) + 48);
+					_putchar((proud / 10) % 10 + 48);
+				} else if (proud <= 99 && proud >= 10)
 					_putchar((proud / 10) + 48);
 				_putchar((proud % 10) + 48);
+
 			}
 			_putchar('\n');
 		}
 	}
-	else
-		return (0);
 }

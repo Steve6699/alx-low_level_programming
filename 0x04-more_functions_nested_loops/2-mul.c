0e35232a034be118f45fc87b663eaf551d95b2bc
Steve6699 <tifaalkhwad@gmail.com>
2023-08-28 15:42:44 -0700

    function multiplies twwo intager number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function multiplies two intager
+ * @a: int number 1
+ * @b: int number 2
+ *
+ * Return: result
+*/
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}

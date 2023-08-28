8a621fbf820ddae2c23ddefab3af926e3c357d6f
Steve6699 <tifaalkhwad@gmail.com>
2023-08-27 17:52:29 -0700

    function add to number intager

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function add to intager number and
+ * return the resulte
+ *
+ * @a: first num
+ * @b: socund num
+ *
+ * Return: result
+*/
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}

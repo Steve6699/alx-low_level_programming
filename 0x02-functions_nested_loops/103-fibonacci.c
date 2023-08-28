991244b1afd26cd560fec79c8b3932edd49c44d0
Steve6699 <tifaalkhwad@gmail.com>
2023-08-27 21:38:49 -0700

    program that finds and prints the sum of the even-valued terms in range to 4000000 , followed by a new line.

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: sum even number in fibtchini
+ * from 0 to 4000000
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+		if (sum % 2 == 0)
+			total += sum;
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0f\n", total);
+	return (0);
+}

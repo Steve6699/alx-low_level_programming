5d86b68c02612c81b3b2e9f0731bdcd28125f151
Steve6699 <tifaalkhwad@gmail.com>
2023-08-30 20:47:58 -0700

    function calcu sqr root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,5 +4,30 @@
  * _sqrt_recursion - that returns the natural
  * square root of a number
  * @n: number input
+ * @v: value to square root
+ * Return: int
+*/
+
+int square_f(int n, int v);
+int _sqrt_recursion(int n)
+{
+	return (square_f(n, 1));
+}
+
+/**
+ * square_f - find square
+ * @n: input number 1
+ * @v: value to square root
  *
- * Return: */
+ * Return: int
+*/
+
+int square_f(int n, int v)
+{
+	if (v * v == n)
+		return (v);
+	else if (v * v < n)
+		return (square_f(n, v + 1));
+	else
+		return (-1);
+}

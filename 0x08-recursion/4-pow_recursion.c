d1adbbf0ae3d4599839078240bc4310caea6212f
Steve6699 <tifaalkhwad@gmail.com>
2023-08-30 20:29:45 -0700

    update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,16 +5,19 @@
  * of x raised to the power of y
  * @x: num base
  * @y: num raised as power
- * Return: int
+ * Return: int v
 */
 
 int _pow_recursion(int x, int y)
 {
+	int v;
+
 	if (y < 0)
 		return (-1);
 	else if (y == 1)
 		return (x);
 	else if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, (y - 1)));
+	v = (x * _pow_recursion(x, y - 1));
+	return (v);
 }
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - that returns the natural
+ * square root of a number
+ * @n: number input
+ *
+ * Return: */

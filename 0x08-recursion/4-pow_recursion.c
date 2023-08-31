73abe8b3e66900f6f6692c02fb5b61e7f447fdf3
Steve6699 <tifaalkhwad@gmail.com>
2023-08-30 20:07:21 -0700

    update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,16 +11,10 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
 	else if (y == 1)
-	{
 		return (x);
-	}
 	else if (y == 0)
-	{
 		return (1);
-	}
 	return (X * _pow_recursion(x, y - 1));
-}	
+}

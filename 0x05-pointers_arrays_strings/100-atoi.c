326ea710473f9179afce3bccce67422e5ece90cf
Steve6699 <tifaalkhwad@gmail.com>
2023-09-04 05:30:10 -0700

    converrt string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,8 +16,8 @@ int _atoi(char *s)
 			si *= -1;
 		else if (*s >= '0' && *s <= '9')
 			n = (n * 10) + (*s - '0');
-		else if (*s > 0)
+		else if (n > 0)
 			break;
 	} while (*s++);
-	return (num * si);
+	return (n * si);
 }

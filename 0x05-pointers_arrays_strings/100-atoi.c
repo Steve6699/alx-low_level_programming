7b99409bc84fcbafe1e622521457e080b8374336
Steve6699 <tifaalkhwad@gmail.com>
2023-09-04 05:27:46 -0700

    converrt string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _atoi - function convert string to intager
+ * @s: pointer to string
+ * Return: int
+*/
+
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+	int si = 1;
+
+	do {
+		if (*s == '-')
+			si *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (*s > 0)
+			break;
+	} while (*s++);
+	return (num * si);
+}

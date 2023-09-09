981e3d7360f47d91c22594ca23671d939e56f489
Steve6699 <tifaalkhwad@gmail.com>
2023-09-09 00:52:42 -0700

    print nuber of arg passed to program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arg passed to program
+ * @argc: number of arg
+ * @argv: array of string arg
+ * Return: 0 if success
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

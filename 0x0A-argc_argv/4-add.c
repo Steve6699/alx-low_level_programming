8f13d28c0caec81877473f9b16fbe2530099de4b
Steve6699 <tifaalkhwad@gmail.com>
2023-09-09 01:52:47 -0700

    adding all int arg and print result

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,10 +15,11 @@ int main(int argc, char *argv[])
 
 	while (--argc)
 	{
-		for (c = argv[argc]; *c; c++)
+		for (a = argv[argc]; *a; a++)
 			if (*a < '0' || *a > '9')
+			{
 				return (printf("Error\n"), 1);
-
+			}
 		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);

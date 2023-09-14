1924fefa2c0dbb1153134265dfea93f803cdf7e0
Steve6699 <tifaalkhwad@gmail.com>
2023-09-14 14:16:00 -0700

    allocate memory to array and set all values zero with malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * *_setmemb - fill a memory with a const byte
+ * @c: pointer to memory
+ * @a: const
+ * @x: int byte to use
+ * Return: pointer
+*/
+
+char *_setmemb(char *c, char a, unsigned int x)
+{
+	int *pt = s;
+
+	while (x--)
+	{
+		*c++ = a;
+	}
+	return (pt);
+}
+
+/**
+ * *_calloc - allocate memory for array on malloc
+ * @nmemb: array len
+ * @size: size of each eleme
+ * Return: pointer
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *s;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(sizeof(int) * nmemb);
+
+	if (s == 0)
+		return (NULL);
+
+	_setmemb(s, 0, sizeof(int) * nmemb);
+
+	return (s);
+}

e1aa835f904a1ea43662230ccf70365bab1df508
Steve6699 <tifaalkhwad@gmail.com>
2023-09-27 19:44:49 -0700

    update

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include <lists.h>
+
+/**
+ * _strLen - find en of str
+ * @x: str
+ * Return: int len
+*/
+
+int _strLen(char *x)
+{
+	int s = 0;
+
+	if (!x)
+		return (0);
+	while (*x++)
+		s++;
+	return (s);
+}
+
+/**
+ * print_list - pr list
+ * @h: pointer
+ * Return: sizt of list
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		printf("[%d] %s", _strLen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		s++:
+	}
+	return (s);
+}

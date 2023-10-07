cdcf11dcaaf18ac5e106679db32f92e1eed6ee55
Steve6699 <tifaalkhwad@gmail.com>
2023-10-06 18:05:48 -0700

    open file and read from it and print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - read tet from file and print is
+ * @filename: the name of file will be read from
+ * @letters: number of letter will be read
+ * Return: byte read or printed
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	ssize_t byte;
+	char bufs[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	f = open(filename, o_RDONLY);
+	if (f == -1)
+		return (0);
+	byte = read(f, &bufs[0], letters);
+	byte = write(STDOUT_FILENO, &bufs[0], byte);
+	close(f);
+	return (byte);
+}

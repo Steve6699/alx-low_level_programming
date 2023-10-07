ce36bbdbb45268152cb2ffb569b667f409cecb6f
Steve6699 <tifaalkhwad@gmail.com>
2023-10-06 19:14:10 -0700

    uo

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+#include <stddef.h>
 
 #define READ_BUF_SIZE 1024
 
@@ -13,4 +15,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* MAIN_H */

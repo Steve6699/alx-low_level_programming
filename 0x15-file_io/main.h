84601b467c7939a5766be11c715592e2f1cc8bb3
Steve6699 <tifaalkhwad@gmail.com>
2023-10-06 17:52:02 -0700

    readme file and main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <Fcntl.h>
+#include <stdio.h>
+
+#define READ_BUF_SIZE 1024
+
+int _putchar(char x);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif

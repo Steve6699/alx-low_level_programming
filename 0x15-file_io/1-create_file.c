31d92f0c08bd2cbedcf9e07d25d097af70f50838
Steve6699 <tifaalkhwad@gmail.com>
2023-10-06 19:08:37 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	f = open(filename, 0_WRONLY | 0_CREAT | 0_TRUNC, S_IRUSR | S_IWUSR);
+	f = open(filename, 0_WRONLY | o_CREAT | o_TRUNC, S_IRUSR | S_IWUSR);
 	if (f == -1)
 		return (-1);
 	if (lenn)
